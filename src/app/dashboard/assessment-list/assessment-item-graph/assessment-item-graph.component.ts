cd69b7241f1c926c1362038997609e17b95ccf44
IKala4I <pershkovladyslav@gmail.com>
2024-06-29 20:11:29 +0300

    + graph and roles are added

--- src/app/dashboard/assessment-list/assessment-item-graph/assessment-item-graph.component.ts
+++ src/app/dashboard/assessment-list/assessment-item-graph/assessment-item-graph.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, Inject, OnDestroy, OnInit} from '@angular/core';
+import {MAT_DIALOG_DATA} from '@angular/material/dialog';
+import {UserService} from 'src/app/services/user.service';
+import {Subject, takeUntil} from 'rxjs';
+import {Graph} from 'src/app/interfaces/graph';
 
 @Component({
   selector: 'auth-assessment-item-graph',
@@ -7,6 +11,24 @@ import { Component } from '@angular/core';
   templateUrl: './assessment-item-graph.component.html',
   styleUrl: './assessment-item-graph.component.scss'
 })
-export class AssessmentItemGraphComponent {
+export class AssessmentItemGraphComponent implements OnInit, OnDestroy {
 
+  graph: Graph;
+  untilSubject$ = new Subject<void>();
+
+  constructor(@Inject(MAT_DIALOG_DATA) private id: number, private userService: UserService) {
+  }
+
+  ngOnInit() {
+    this.userService.getUserAssessmentsGraph(this.id)
+      .pipe(
+        takeUntil(this.untilSubject$)
+      )
+      .subscribe(graphData => this.graph = graphData);
+  }
+
+  ngOnDestroy() {
+    this.untilSubject$.next();
+    this.untilSubject$.complete();
+  }
 }
--- src/app/dashboard/assessment-list/assessment-list.component.ts
+++ src/app/dashboard/assessment-list/assessment-list.component.ts
@@ -1,6 +1,10 @@
 import {Component, Input} from '@angular/core';
 import {Assessment} from 'src/app/interfaces/assessment';
 import {MatTableModule} from '@angular/material/table';
+import {MatDialog} from '@angular/material/dialog';
+import {
+  AssessmentItemGraphComponent
+} from 'src/app/dashboard/assessment-list/assessment-item-graph/assessment-item-graph.component';
 
 @Component({
   selector: 'auth-assessment-list',
@@ -16,7 +20,12 @@ export class AssessmentListComponent {
   @Input() assessments: Assessment[];
   displayedColumns = ['image', 'name', 'usersResolved', 'active'];
 
-  showGraph(event: Assessment) {
-    console.log(event);
+  constructor(private dialog: MatDialog) {
+  }
+
+  showGraph(id: number) {
+    this.dialog.open(AssessmentItemGraphComponent, {
+      data: id
+    });
   }
 }
